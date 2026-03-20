7feb913f852fe87a4835f32b78657c0e478c872f
Ivan <myaumurzik@gmail.com>
2026-03-21 00:40:48 +0300

    Deleted previous project from this branch

--- Shaders/src/main/java/com/engine/Scene/Objects/Plane.java
+++
@@ -1,9 +0,0 @@
-package com.engine.Scene.Objects;
-
-import org.joml.Vector3f;
-
-public class Plane extends Object {
-    public Plane(Vector3f pos, Vector3f color) {
-        super(pos, color);
-    }
-}
